ceb11538b6abdca2be42b59b133d66ae41d7300b
6kamens <slil.puangpoom@gmail.com>
2021-08-01 20:51:33 +0700

    fix build

--- src/view/Home/style.tsx
+++ src/view/Home/style.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { Col } from "react-bootstrap";
 import mainBg from "../../asset/images/main-bg.jpg";
 import { DropdownButton } from "react-bootstrap";
 
